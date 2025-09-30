d333898e88a99f08cfbd5fe318bd95b0d9c4a003
unknown <aguraarnold@gmail.com>
2025-09-30 16:51:18 +0800

    app setup this commit contains: - added shadcn setup - root layout and constants

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: 'NexStore',
+  description: 'modern e-commerce store built in Next.js',
+};
+
+const Homepage = () => {
+  return <>NexStore</>;
+}
+ 
+export default Homepage;
